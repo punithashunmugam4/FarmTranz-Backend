58078b21582af0a4ff7dc0582f6f2a15f2310885
punithashunmugam4 <punithashunmugam4@gmail.com>
2025-08-13 22:05:22 +0530

    latest railway

--- middleware/sqlConnect.js
+++ middleware/sqlConnect.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 var con = mysql.createConnection({
-  host: "103.26.110.189",
-  user: "shunmugam",
-  password: "punitha",
-  database: "farmTranz",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQLPORT,
 });
 con.connect((err) => {
   if (err) throw err;
@@ -14,3 +16,24 @@ module.exports = con;
 // host: "127.0.0.1",
 // user: "root",
 // password: "shunmugam1502"
+
+// host: "103.26.110.189",  103.160.240.150
+// user: "punitha",
+// password: "punitha1502",
+// database: "farmTranz",
+
+//  host: "192.168.1.6",
+//   user: "shunmugam",
+//   password: "punitha",
+//   database: "farmTranz",
+
+//  host: process.env.DB_HOST,
+//  user:process.env.DB_USER,
+//  password:process.env.DB_PASS,
+//  database:process.env.DB_NAME
+
+// host: "mysql.railway.internal",
+// user: "root",
+// password: "YMYUqnfDbCiYIyJBHuZlOZJauEOCFyAU",
+// database: "railway",
+// port: "3306",
